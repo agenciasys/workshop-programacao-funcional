aeff31ce790ab3dd84ac4777ca9b7b2ba759a68a
thyagorafaell <thyagorafael012@gmail.com>
2015-05-15 14:01:01 -0300

    JS

--- index.php
+++ index.php
@@ -1,43 +1,17 @@
 <?php
-$teams = [
-	[
-		'name' => 'São Paulo',
-		'city' => 'São Paulo',
-		'state' => 'SP',
-	],
-	[
-		'name' => 'Corinthians',
-		'city' => 'São Paulo',
-		'state' => 'SP',
-	],
-	[
-		'name' => 'Coritiba',
-		'city' => 'Curitiba',
-		'state' => 'PR',
-	],
-	[
-		'name' => 'Joinville',
-		'city' => 'Joinville',
-		'state' => 'SC',
-	],
-	[
-		'name' => 'Santos',
-		'city' => 'Santos',
-		'state' => 'SP',
-	]
-];
-
-$players = [
-	[
-		'name' => 'Rogerio Ceni',
-		'teams' => ['São Paulo']
-	],
-	[
-		'name' => 'Socrates',
-		'teams' => ['Corinthians', 'Santos']
-	],
-	[
-		'name' => 'Nardella',
-		'teams' => ['Joinville']
-	]
-];
+	require 'base.php';
+?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="language" content="pt">
+</head>
+<body>
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+	<script>
+		(function(w) {
+			w.teams = w.JSON.parse('<?= json_encode($teams); ?>');
+			w.players = w.JSON.parse('<?= json_encode($players); ?>');
+		}(window));
+	</script>
+	<script src="main.js"></script>
+</body>
