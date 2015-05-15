5b2729f41d45be0512e3dda82200e80d86c6dddd
thyagorafaell <thyagorafael012@gmail.com>
2015-05-15 13:06:56 -0300

    Workshop.

--- fp.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('display_errors', true);
-error_reporting(-1);
-
-require_once 'vendor/autoload.php';
-$_ = new \__();
-
-print_r($_->map(array(1, 2, 3), function($n) { return $n * 2; }));
